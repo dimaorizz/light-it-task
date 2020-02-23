aa84ce1b5150e3f402f23f89b0f2e500728843fc
Orizz <dimaorizz@gmail.com>
2020-02-23 14:51:15 +0200

    added user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+// Libs
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: { 
+        type: Number, // 0 - seller; 1 - cashier; 2 - accountant;
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('users', UserSchema);
